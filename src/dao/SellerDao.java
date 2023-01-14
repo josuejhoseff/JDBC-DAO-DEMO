e75af88e8362744d1c362332a5fb68ad7e960c44
Josue Jhoseff <josuejhosefff@gmail.com>
2023-01-14 10:06:51 -0300

    DepartmentDao e SellerDao

---
+++ src/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.List;
+
+import entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller seller);
+	void update(Seller seller);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+
+}
