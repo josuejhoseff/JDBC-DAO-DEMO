312bcd387c2fb21f6773882d7e930717f16dca8f
Josue Jhoseff <josuejhosefff@gmail.com>
2023-01-14 09:56:49 -0300

    Entidades seller e department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Date;
+
+import entities.Department;
+import entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department dep = new Department(1, "Books");
+		
+		Seller seller = new Seller(2, "Lucas", "lucas@gmail.com", new Date(), 3000.0, dep);
+		
+		System.out.println(dep);
+		System.out.println(seller);
 	}
 
 }
