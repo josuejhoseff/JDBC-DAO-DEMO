9e376276f354e72024389c36dc5a53e54891ec17
Josue Jhoseff <josuejhosefff@gmail.com>
2023-01-19 11:47:13 -0300

    Teste SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 import dao.DaoFactory;
 import dao.SellerDao;
@@ -11,15 +13,45 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Books");
+Scanner sc = new Scanner(System.in);
 		
-		Seller seller = new Seller(2, "Lucas", "lucas@gmail.com", new Date(), 3000.0, dep);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(dep);
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		SellerDao sellerDao = new DaoFactory().createSellerDao();
-		System.out.println(sellerDao.findById(3));
+		System.out.println("\n=== TEST 2: seller findByDepartment =====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n=== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+
+		System.out.println("\n=== TEST 6: seller delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 }
